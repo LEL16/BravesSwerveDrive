221c770023e8e34c369a9af3fc3e6ebd6e9f75e0
9016Lysol <elvitigalalis@gmail.com>
2023-06-01 18:32:58 -0400

    Fixed up an autobalance issue

--- src/main/java/frc/robot/Commands/Drivetrain/AutoCommands/AutoBalance.java
+++ src/main/java/frc/robot/Commands/Drivetrain/AutoCommands/AutoBalance.java
@@ -6,35 +6,46 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Subsystems.DrivetrainSubsystem;
 
 public class AutoBalance extends CommandBase {
-    DrivetrainSubsystem m_drivetrainSubsystem;
+    private final DrivetrainSubsystem m_drivetrainSubsystem;
     
-    PIDController m_rollPIDController;
-    double m_yVelocity;
+    private final PIDController m_rollPIDController;
+    private final PIDController m_pitchPIDController;
+    private static final double m_kP = 0.05;
+    private static final double m_kI = 0.0;
+    private static final double m_kD = 0.0;
+    private static final double m_degreesTolerance = 1.0;
+
+    private double m_yVelocity;
+    private double m_xVelocity;
 
     public AutoBalance(DrivetrainSubsystem drivetrainSubsystem) {
         m_drivetrainSubsystem = drivetrainSubsystem;
+
+        m_rollPIDController = new PIDController(m_kP, m_kI, m_kD);
+        m_pitchPIDController = new PIDController(m_kP, m_kI, m_kD);
+
+        m_rollPIDController.setTolerance(m_degreesTolerance);
+        m_pitchPIDController.setTolerance(m_degreesTolerance);
 
         addRequirements(m_drivetrainSubsystem);
     }
 
     @Override
     public void initialize() {
-        m_rollPIDController = new PIDController(0.01, 0, 0);
+        m_rollPIDController.setSetpoint(0);
+        m_pitchPIDController.setSetpoint(0);
     }
 
-    /* IMPORTANT: Robot has to be approaching the charging station from the left side! */
     @Override
     public void execute() {
+        m_xVelocity = MathUtil.applyDeadband(m_pitchPIDController.calculate(m_drivetrainSubsystem.getPitch()), 0.01);
         m_yVelocity = MathUtil.applyDeadband(m_rollPIDController.calculate(m_drivetrainSubsystem.getRoll()), 0.01);
-        m_drivetrainSubsystem.drive(0, m_yVelocity, 0, true);
+        m_drivetrainSubsystem.drive(-m_xVelocity, m_yVelocity, 0, true);
     }
 
     @Override
     public boolean isFinished() {
-        if (Math.abs(m_drivetrainSubsystem.getRoll()) > 3 ) {
-            return false;
-        }
-        return true;
+        return m_rollPIDController.atSetpoint() && m_pitchPIDController.atSetpoint();
     }
 
     @Override
